15295d0b5000cfb691a1fee8659270a54a7d5158
Cameron Padron <cameronpadron19@live.com>
2018-11-29 00:18:07 -0800

    Add files via upload

---
+++ LWTech.CameronPadron.Assignment8/LWTech.CameronPadron.Assignment8/Histogram.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LWTech.CameronPadron.Assignment8
+{
+	class Histogram
+	{
+		private int width;
+		private int maxBarWidth;
+		private int maxLabelWidth;
+		private int minValue;
+		private List<KeyValuePair<string, int>> bars;
+
+		public Histogram(List<string> data, int width = 80, int maxLabelWidth = 10, int minValue = 0)
+		{
+			this.width = width;
+			this.maxLabelWidth = maxLabelWidth;
+			this.minValue = minValue;
+			this.maxBarWidth = width - maxLabelWidth - 2;   // -2 for the space and pipe separator
+
+			var barCounts = new Dictionary<string, int>();
+
+			foreach (string item in data)
+			{
+				if (barCounts.ContainsKey(item))
+					barCounts[item]++;
+				else
+					barCounts.Add(item, 1);
+			}
+
+			this.bars = new List<KeyValuePair<string, int>>(barCounts);
+
+		}
+
+		public void Sort(Comparison<KeyValuePair<string, int>> f)
+		{
+			bars.Sort(f);
+		}
+
+		public override string ToString()
+		{
+			string s = "";
+			string blankLabel = "".PadRight(maxLabelWidth);
+
+			int maxValue = 0;
+			foreach (KeyValuePair<string, int> bar in bars)
+			{
+				if (bar.Value > maxValue)
+					maxValue = bar.Value;
+			}
+
+			foreach (KeyValuePair<string, int> bar in bars)
+			{
+				string key = bar.Key;
+				int value = bar.Value;
+
+				if (value >= minValue)
+				{
+					string label;
+					if (key.Length < maxLabelWidth)
+						label = key.PadLeft(maxLabelWidth);
+					else
+						label = key.Substring(0, maxLabelWidth);
+
+					int barSize = (int)(((double)value / maxValue) * maxBarWidth);
+					string barStars = "".PadRight(barSize, '*');
+
+					s += label + " |" + barStars + " " + value + "\n";
+				}
+			}
+
+			string axis = blankLabel + " +".PadRight(maxBarWidth + 2, '-') + "\n";
+			s += axis;
+
+			return s;
+		}
+	}
+}
+
---
+++ LWTech.CameronPadron.Assignment8/LWTech.CameronPadron.Assignment8/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace LWTech.CameronPadron.Assignment8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Boeing and Airbus Models Still in Flight");
+			Console.WriteLine("=========================================================================================================");
+
+
+			string line = "";
+			string[] tokens= null;
+
+			List<string> planeTypes = new List<string>();
+			List<string> AirBusAndBoeing = new List<string>();
+
+			
+
+			try
+			{
+				WebClient client = new WebClient();
+				Stream stream = client.OpenRead("https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?fTypQN=");
+
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					while (!reader.EndOfStream)
+					{
+					    line = reader.ReadLine();
+						line = line.Replace("\"Type\":","|");
+					    tokens = line.Split('|');
+						//Console.WriteLine(line); // For Step 2 of assignment
+						
+						
+					}
+
+					
+				}
+			}
+			catch (IOException exception)
+			{
+				Console.WriteLine("Something Happened... " + exception.Message);
+				Console.WriteLine("Terminating Program...");
+				return;
+			}
+
+
+			
+			
+			foreach (string s in tokens)
+			{
+				
+				planeTypes.Add(s.Substring(0,5).Replace("\"", "")); // Adding our plane models to list. 
+				
+			}
+
+
+			foreach (string s in planeTypes)
+			{
+
+				if (s.Contains("B7")) // adding our boeing and airbus models to their own list. 
+				{
+					string boeing = s.Remove(s.Length - 1, 1) + "7";
+					AirBusAndBoeing.Add(boeing);
+					//Console.WriteLine(boeing);
+
+				}
+				if (s.Contains("A3"))
+				{
+					string airbus = s.Remove(s.Length - 1, 1) + "0";
+					AirBusAndBoeing.Add(airbus);
+					//Console.WriteLine(airbus);
+
+				}
+
+				//Console.WriteLine(s);
+				//Console.ReadLine(); //Testing that it's splitting at the right spot
+			}
+
+			Histogram PlaneTypeHistogram = new Histogram(AirBusAndBoeing, width: 100, maxLabelWidth: 5);
+			PlaneTypeHistogram.Sort((x, y) => y.Value.CompareTo(x.Value));
+			Console.WriteLine(PlaneTypeHistogram);
+
+
+			Console.ReadLine();
+
+		}
+	}
+}
